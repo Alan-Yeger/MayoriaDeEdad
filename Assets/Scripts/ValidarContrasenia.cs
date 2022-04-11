f5a7f9ccb35866c1e4ab8e113b0ab2a06b64f514
Alan-Yeger <alanyeger1@gmail.com>
2022-04-11 20:06:06 -0300

    Initial commit

---
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textomsj;
+    public GameObject cartelitomsj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartelitomsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelitomsj.SetActive(true);
+            textomsj.text = "Bienvenido";
+            
+            
+        }
+        else
+        {
+            cartelitomsj.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
+        }
+    }
+
+}
